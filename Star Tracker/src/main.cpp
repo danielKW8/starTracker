ece913e6d93ad2160066291452db85060b25c5cc
Edward P <edwardprajna@gmail.com>
2026-06-27 12:29:14 +0700

    Added Knob, need to review the logic. Added adjust speed function that edits length for one revolution

--- Star Tracker/src/main.cpp
+++ Star Tracker/src/main.cpp
@@ -14,13 +14,14 @@ namespace StepperData {
   //Timing
   const int timeInterval = 10; // Time interval in seconds for the stepper to move
   const float siderealDay = 86164.0905; // Sidereal day in seconds  
+  float lengthOfOneRevolution = siderealDay; // Length of one revolution in seconds
 }
 
 // ----------------------------------- CALCULATIONS -----------------------------------
 namespace calculations {
-  const float degreesPerSecond = (360.0 / StepperData::siderealDay) * StepperData::gearRatio; // Calculate degrees per second based on sidereal day and gear ratio
+  float degreesPerSecond = (360.0 / StepperData::lengthOfOneRevolution) * StepperData::gearRatio; // Calculate degrees per second based on sidereal day and gear ratio
 
-  const float degreesPerInterval = degreesPerSecond * StepperData::timeInterval; // Calculate degrees to move in the given time interval
+  float degreesPerInterval = degreesPerSecond * StepperData::timeInterval; // Calculate degrees to move in the given time interval
 
   //MOTOR SPEED
   const float stepsToMovePerInterval = (degreesPerInterval * StepperData::steps) / 360.0; // Calculate the speed in steps to move per interval
@@ -30,6 +31,23 @@ namespace calculations {
 FastAccelStepperEngine engine = FastAccelStepperEngine();
 
 FastAccelStepper *stepper;
+
+// ------------------------------------ Increment & Decrement Knobs -----------------------------------
+// Increment and decrement the speed of the stepper motor using knobs
+namespace KnobData {
+  const int hoursKnobPin = A0; // Knob connected to analog pin A0
+  const int minutesKnobPin = A1; // Knob connected to analog pin A1
+}
+
+// function to recieve input of knob and adjust speed accordingly
+void adjustSpeedWithKnob() {
+  // Read the value from the knob (potentiometer)
+  int knobValue = analogRead(KnobData::hoursKnobPin); // Knob connected to analog pin A0
+
+  int newHours = map(knobValue, 0, 1023, 0, 50); // Map the knob value to a range of 0 to 50 hours
+
+  StepperData::lengthOfOneRevolution = map(newHours, 0, 50, 0, 180000); // Map the new hours to a range of 0 to 180,000 seconds (50 hours)
+}
 
 void setup() {
   Serial.begin(9600);
