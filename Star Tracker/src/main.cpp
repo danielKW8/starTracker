84583a5bae9f34a9f887a3eef742cd5ad261b36f
Edward P <edwardprajna@gmail.com>
2026-06-27 09:55:05 +0700

    Fixed main, TO DO: speed of motor config

--- Star Tracker/src/main.cpp
+++ Star Tracker/src/main.cpp
@@ -2,21 +2,30 @@
 #include <FastAccelStepper.h>
 
 // ----------------------------------- STEPPER DRIVER CONFIG!!!!! -----------------------------------
+namespace StepperData {
+  //Pin Assignments
+  const int stepPin = 2; 
+  const int dirPin = 3; 
 
-//MS1 200, MS2 400, MS1+MS2 800, MS1+MS2+MS3 1600, 1/16 = 3200
-int steps = 3200;
-const int stepPin = 2; // Pin connected to the step input of the stepper driver
-const int dirPin = 3;  // Pin connected to the direction input of the stepper driver
+  //stepper settings
+  const int steps = 3200;
+  const int gearRatio = 200; 
 
-// ----------------------------------- IMPORTANT SETTINGS -----------------------------------
-
-int timeInterval = 10; // Time interval in seconds for the stepper to move
-int GearRatio = 200;
-float siderealDay = 86164.0905; // Sidereal day in seconds
+  //Timing
+  const int timeInterval = 10; // Time interval in seconds for the stepper to move
+  const float siderealDay = 86164.0905; // Sidereal day in seconds  
+}
 
 // ----------------------------------- CALCULATIONS -----------------------------------
-int degreesPerInterval = degreesPerSecond * timeInterval; // Calculate degrees to move in the given time interval
-int degreesPerSecond = (360 / siderealDay) * GearRatio; // Calculate degrees per second based on sidereal day and gear ratio
+namespace calculations {
+  const float degreesPerSecond = (360.0 / StepperData::siderealDay) * StepperData::gearRatio; // Calculate degrees per second based on sidereal day and gear ratio
+
+  const float degreesPerInterval = degreesPerSecond * StepperData::timeInterval; // Calculate degrees to move in the given time interval
+
+  const float stepsToMovePerInterval = (degreesPerInterval * StepperData::steps) / 360.0; // Calculate the speed in steps to move per interval
+}
+
+// TO DO: ADD MOTOR SPEED DEFINITION BASED ON STEPSTOMOVEPERINTERVAL
 
 // ----------------------------------- Initialize Engine -----------------------------------
 FastAccelStepperEngine engine = FastAccelStepperEngine();
@@ -28,10 +37,10 @@ void setup() {
 
   engine.init();
 
-  stepper = engine.stepperConnectToPin(stepPin);
+  stepper = engine.stepperConnectToPin(StepperData::stepPin);
 
   if (stepper) {
-    stepper->setDirectionPin(dirPin);
+    stepper->setDirectionPin(StepperData::dirPin);
     stepper->setEnablePin(4); // Optional: Set an enable pin if your driver has one
     stepper->setAutoEnable(true); // Automatically enable the stepper when stepping
 
@@ -45,21 +54,21 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  degreesToSteps(degreesPerInterval);
+  degreesToSteps(calculations::degreesPerInterval);
 }
 
 // put function definitions here:
 //run stepper degrees to steps
 void degreesToSteps(int degrees) {
-  int stepsToMove = (degrees * steps) / 360;
+  int stepsToMove = (degrees * StepperData::steps) / 360;
   stepper->move(stepsToMove);
   Serial.print("Moving stepper " + String(stepsToMove) + " steps (" + String(degrees) + " degrees).");
 
 
   // Wait until the stepper has finished moving
   while (stepper->isRunning()) {
-    delay(timeInterval * 1000); //wait timeInterval seconds
-    Serial.println("Interval: " + String(timeInterval) + "seconds");
+    delay(StepperData::timeInterval * 1000); //wait timeInterval seconds
+    Serial.println("Interval: " + String(StepperData::timeInterval) + "seconds");
     // Wait until the stepper has finished moving
   }
 }
